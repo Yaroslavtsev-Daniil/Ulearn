c27761e88b31cd041fadb6b65d0c206a3e8918a4
Yaroslavtsev Daniil <yaroslavtsev.daniil@gmail.com>
2021-04-23 15:10:38 +0300

    upload project try2

---
+++ ConsoleApp4/Lesson1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lessons
+{
+    class Lesson1
+    {
+        /*Реализуйте метод GetMinX для нахождения такого числа x, при котором кривая, заданная уравнением y(x) = ax^2 + bx + c  принимает минимальное значение.
+         * Метод должен принимать неотрицательный коэффициент a, а также произвольные коэффициенты b и c, и, если решение существует,
+         * возвращать строковое представление искомого x, а иначе — строку Impossible.*/
+        private static string GetMinX(double a, double b, double c)
+        {
+            if (a < 0)
+            {
+                return "Impossible";
+            }
+            if (a == 0 && b > 0)
+            {
+                return "Impossible";
+            }
+            return ((-b) / (2 * a)).ToString();
+        }
+        public static void Tests()
+        {
+            Console.WriteLine(GetMinX(1, 2, 3));
+            Console.WriteLine(GetMinX(0, 3, 2));
+            Console.WriteLine(GetMinX(1, -2, -3));
+            Console.WriteLine(GetMinX(5, 2, 1));
+            Console.WriteLine(GetMinX(4, 3, 2));
+            Console.WriteLine(GetMinX(0, 4, 5));
+
+            // А в этих случаях решение существует:
+            Console.WriteLine(GetMinX(0, 0, 2) != "Impossible");
+            Console.WriteLine(GetMinX(0, 0, 0) != "Impossible");
+        }
+    }
+}
---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace Lessons
+{
+    class ControlPrograms
+    {
+        ///Для каждой задачи вызываем свои методы
+
+        ///Флаг для работы программы
+        static string power = "1";
+        static void Main()
+        {
+            do
+            {
+                //Выбираем что запустить
+                ChooseLesson();
+
+                //Запрашиваем повтор
+                AskRestart();
+            }
+            while (power == "1");
+        }
+        static void ChooseLesson()
+        {
+            Console.WriteLine(
+                "Что запустить?\nВарианты:\n" +
+                "Первое - 1\n");
+                
+            int Lesson = int.Parse(Console.ReadLine());
+            Console.Clear();
+            switch (Lesson)
+            {
+                case 1: Lesson1.Tests(); break;
+                
+                case 0:
+                    Console.Clear();
+                    Console.WriteLine("Работа завершена");
+                    break;
+            }
+        }
+        static void AskRestart()
+        {
+            Console.WriteLine("\nВыбрать задачу - 1\n" + "Выйти 0");
+            power = Console.ReadLine();
+            Console.Clear();
+            Console.WriteLine("Работа завершена");
+        }
+        /// <summary>
+        /// Вызов решения задания №1
+        /// </summary>
+        
+    }
+}

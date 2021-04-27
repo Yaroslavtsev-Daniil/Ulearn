0ad8cdb3c3632dbb62f9a006b79c9429060701de
Yaroslavtsev Daniil <yaroslavtsev.daniil@gmail.com>
2021-04-27 12:48:12 +0300

    upload Pluralize solution

--- Pluralize/PluralizeTask.cs
+++ Pluralize/PluralizeTask.cs
@@ -4,8 +4,19 @@
 	{
 		public static string PluralizeRubles(int count)
 		{
-			// Напишите функцию склонения слова "рублей" в зависимости от предшествующего числительного count.
-			return "руб.";
+			if (count % 10 >= 5 || count % 10 == 0)
+			{
+				return "рублей";
+			}
+			else if (count % 100 >= 11 && count % 100 <= 14)
+			{
+				return "рублей";
+			}
+			else if (count % 10 == 1)
+			{
+				return "рубль";
+			}
+			else return "рубля";
 		}
 	}
 }

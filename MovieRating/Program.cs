bca389bd49f3a1316554fda74152a06e727da7e3
Yaroslavtsev Daniil <yaroslavtsev.daniil@gmail.com>
2021-11-19 23:52:55 +0300

    Задача решена

--- MovieRating/Program.cs
+++ MovieRating/Program.cs
@@ -6,7 +6,17 @@ namespace MovieRating
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            double Cp1 = 9.5;
+            double Cp2 = 2.0;
+            int N = 12;
+            double sum = Cp1 * N;
+
+            int Rating(double sum, double Cp2, int N)
+            {
+                if (Math.Round(sum++ / N++, 1) == Cp2) return N-1;
+                return Rating(sum, Cp2, N);
+            }
+            Console.WriteLine(Rating(sum, Cp2, N)-N);
         }
     }
 }

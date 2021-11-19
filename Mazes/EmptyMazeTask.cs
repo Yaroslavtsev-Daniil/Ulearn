e60128d4296214f0b3ecf46164b3387fcce5a11c
Yaroslavtsev Daniil <yaroslavtsev.daniil@gmail.com>
2021-11-20 02:31:05 +0300

    Создание проекта и решение Задачи с Пустыми лабиринтами

---
+++ Mazes/EmptyMazeTask.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Mazes
+{
+	public static class EmptyMazeTask
+	{
+		public static void MoveOut(Robot robot, int width, int height)
+		{
+			MoveRight(robot, width);
+			MoveDown(robot, height);
+		}
+        private static void MoveDown(Robot robot, int height)
+        {
+			for (int i = 1; i < height - 2; i++)
+			{
+				robot.MoveTo(Direction.Down);
+			}
+		}
+        private static void MoveRight(Robot robot, int width)
+        {
+			for (int i = 1; i < width - 2; i++)
+			{
+				robot.MoveTo(Direction.Right);
+			}
+		}
+    }
+}

2961706d462722318ebe0423a02a67cc3b36405a
Yaroslavtsev Daniil <yaroslavtsev.daniil@gmail.com>
2021-11-21 11:24:47 +0300

    Корректировка цикла в Методе движения и связанные доработки

--- Mazes/SnakeMazeTask.cs
+++ Mazes/SnakeMazeTask.cs
@@ -6,17 +6,17 @@ namespace Mazes
 	{
 		public static void MoveOut(Robot robot, int width, int height)
 		{
-			Move(robot, width - 2, Direction.Right);
+			Move(robot, width - 3, Direction.Right);
 			for (int i = 1; i < ((height - 1) / 2); i++)
             {
-				Move(robot, 3, Direction.Down);
-				if (i%2 == 1) Move(robot, width - 2, Direction.Left);
-				if (i % 2 == 0) Move(robot, width - 2, Direction.Right);
+				Move(robot, 2, Direction.Down);
+				if (i%2 == 1) Move(robot, width - 3, Direction.Left);
+				if (i % 2 == 0) Move(robot, width - 3, Direction.Right);
 			}
 		}
 		private static void Move(Robot robot, int distance, Direction dir)
 		{
-			for (int i = 1; i < distance; i++) robot.MoveTo(dir);
+			for (int i = 0; i < distance; i++) robot.MoveTo(dir);
 		}
 	}
 }

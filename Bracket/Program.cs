20d4959c81118db09a171250833f09961706ec3a
Yaroslavtsev Daniil <yaroslavtsev.daniil@gmail.com>
2021-11-22 12:25:59 +0300

    Решение с уровнем вложенности

--- Bracket/Program.cs
+++ Bracket/Program.cs
@@ -7,16 +7,42 @@ namespace Bracket
         static void Main(string[] args)
         {
             string brackets1 = "()()()))(()))(()()";
-            string brackets2 = "()(((()()())())())";
+            string brackets2 = "()(((()(())())())())";
 
-            Console.WriteLine($"brackets1 - {CheckBrackets(brackets1)}");
-            Console.WriteLine($"brackets2 - {CheckBrackets(brackets2)}");
+            Console.Write($"brackets1 - {iSBracketsCorrect(brackets1)}");
+            if (iSBracketsCorrect(brackets1)) Console.Write($"; Lvl: {CaclLvl(brackets1)}");
+            Console.WriteLine();
+            Console.Write($"brackets2 - {iSBracketsCorrect(brackets2)}");
+            if (iSBracketsCorrect(brackets2)) Console.Write($"; Lvl: {CaclLvl(brackets2)}");
+            Console.Write($"; Lvl: {CaclLvl(brackets1)}");
         }
 
-        private static bool CheckBrackets(string s)
+        private static int CaclLvl(string s)
+        {
+            char[] br = s.ToCharArray();
+            int lvl = 0;
+            int maxlvl = 0;
+            
+            for (int i = 0; i < br.Length; i++)
+            {
+                if (br[i] == '(')
+                {
+                    lvl++;
+                    if (lvl > maxlvl) maxlvl++;
+                }
+                else lvl--;
+            }
+            return maxlvl;
+        }
+
+        private static bool iSBracketsCorrect(string s)
         {
             string subString = "()";
-            while (s.Length > 1 && s.IndexOf(subString) >= 0) s = s.Remove(s.IndexOf(subString), 2);
+            while (s.Length > 1 && s.IndexOf(subString) >= 0)
+            {
+                s = s.Remove(s.IndexOf(subString), 2);
+            }
+
 
             if (s.Length == 0) return true;
             else return false;
